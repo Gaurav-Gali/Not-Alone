ba93c122cda0139b981b47bb71a5a847a650f1bb
Krishnaa-Gopi1 <krishnaagopi.dev@gmail.com>
2025-04-18 22:54:49 +0530

    added basic news feature

--- app/(pages)/news/page.tsx
+++ app/(pages)/news/page.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
+import NewsCard from "@/components/pages/NewsCard";
+
+
+type Article = {
+    title: string;
+    summary: string;
+    link: string;
+};
+
 
 const News = () => {
-    return <div>News</div>;
+    const [newsData, setNewsData] = useState<Article[]>([]);
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                const response = await fetch("http://localhost:8000/api/get_news/");
+                const data = await response.json();
+                setNewsData(data.articles); // assuming your Django API returns { articles: [...] }
+            } catch (error) {
+                console.error("Failed to fetch news:", error);
+            }
+        };
+
+        fetchNews();
+    }, []);
+
+    return (
+        <div className="flex  justify-around flex-wrap gap-4 ">
+            {newsData.map((cardData, index) => (
+                <NewsCard
+                    key={index}
+                    title={cardData.title}
+                    summary={cardData.summary}
+                    site={new URL(cardData.link).hostname}
+                    url={cardData.link}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default News;
---
+++ components/pages/NewsCard.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+
+export default function NewsCard({
+    title,
+    summary,
+    site,
+    url,
+  }: {
+    title: string;
+    summary: string;
+    site: string;
+    url: string;
+  }) {
+    return (
+      <div
+        className="border rounded-xl p-5 min-w-[300px] max-w-[400px] max-h-[400px] bg-gray-100 shadow-md  transition-transform duration-200 cursor-pointer hover:-translate-y-1 hover:shadow-lg"
+      >
+        <h2 className="text-xl font-bold text-gray-800 mb-3">{title}</h2>
+        <p className="text-base text-gray-700 mb-4 leading-relaxed  text-sm line-clamp-2">{summary}</p>
+        <div className="flex justify-between items-center text-sm">
+          <span className="text-gray-500 italic">{site}</span>
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-bold text-blue-600 hover:text-blue-800 transition-colors duration-200"
+          >
+            Read more
+          </a>
+        </div>
+      </div>
+    );
+  }
+  
