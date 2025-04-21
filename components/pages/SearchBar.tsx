177b762f3c253c21bff8442510b370c771a7a0a3
Krishnaa-Gopi1 <krishnaagopi.dev@gmail.com>
2025-04-21 11:56:17 +0530

    final global Searchbar

--- components/pages/SearchBar.tsx
+++ components/pages/SearchBar.tsx
@@ -38,32 +38,7 @@ const SearchBar = ({ onSearch }: SearchBarProps) => {
   if (shouldHideSearch) return null;
 
   return (
-    <div className="flex gap-3 items-center pr-7">
-      <div className="relative flex-1">
-        <Input
-          id={id}
-          className="peer pe-9 ps-9 h-10 text-gray-600 rounded-full shadow-none border focus-visible:ring-1 focus-visible:ring-gray-300"
-          placeholder="Search..."
-          type="search"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-        />
-        <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80">
-          {isLoading ? (
-            <LoaderCircle className="animate-spin" size={16} strokeWidth={2} />
-          ) : (
-            <Search size={16} strokeWidth={2} />
-          )}
-        </div>
-        <button
-          className="absolute inset-y-0 right-0 px-4 rounded-e-full text-sm font-medium bg-gray-200 hover:bg-gray-300 transition-colors"
-          onClick={handleSearch}
-        >
-          Go
-        </button>
-      </div>
-
+    <div className="flex gap-3 items-center pr-7 justify-end">
       <CreateModal>
         <Button className="w-full flex gap-1 items-center py-3 rounded-full hover:opacity-[95%] border-neutral-50 bg-gradient-to-r from-blue-500 to-indigo-500">
           <BadgePlus className="w-4" /> Create
